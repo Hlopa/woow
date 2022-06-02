a5b299f23027cf70c1284b137b74404ada7ea032
Olga Barishevskaya <bariolga@yandex.ru>
2022-06-02 23:58:00 +0300

    header adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -8,14 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   setInterval(() => {
-
     //скролл и анимация
     let isScrolling = false;
     let featuresFirst = document.querySelector('.features_first');
     let featuresFirstCat = featuresFirst.querySelector('.features_first-cat');
     let featuresFirstEye = featuresFirst.querySelector('.features_first-cat-eye');
     let featuresFirstEyeLines = featuresFirst.querySelector('.features_first-cat-eye-lines');
-    
+
     window.addEventListener("scroll", throttleScroll, false);
 
     function throttleScroll(e) {
@@ -35,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
       let height = elementBoundary.height - num;
       return ((top + height >= 0) && (height + window.innerHeight >= bottom));
     }
-    
+
     function addClasses(elem, arr) {
       arr.forEach(clas => {
         elem.classList.add(`${clas}`)
@@ -43,23 +42,56 @@ document.addEventListener("DOMContentLoaded", () => {
       return elem
     }
 
-    function deleteClasses(elem, arr) {
-      arr.forEach(clas => {
-        elem.classList.remove(`${clas}`)
-      })
-      return elem
-    }
-    
     document.addEventListener("DOMContentLoaded", scrolling, false);
 
     function scrolling(e) {
       if (isPartiallyVisible(featuresFirst, 150)) {
         addClasses(featuresFirstCat, ['animate__animated', 'animate__fadeInRight', 'animate__delay-1s']);
         addClasses(featuresFirstEye, ['animate__animated', 'animate__bounceIn', 'animate__delay-2s']);
-        addClasses(featuresFirstEyeLines, ['animate__animated', 'animate__bounceIn',  'animate__delay-3s',]);
+        addClasses(featuresFirstEyeLines, ['animate__animated', 'animate__bounceIn', 'animate__delay-3s',]);
       }
     }
-  })
+
+
+    //плавный скролл при клике на меню
+
+    const menuList = document.querySelector('.header__menu-list');
+
+    const getSmoothScroll = () => {
+      const ancors = menuList.querySelectorAll('a');
+     
+      const getScroll = (element) => {
+        element.addEventListener('click', function (e) {
+          e.preventDefault();
+          const blockID = element.getAttribute('href');
+          document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          });
+          burger.classList.toggle('menu-burger__active');
+          menuBody.classList.toggle('header__menu-body--show');
+        })
+      }
+
+      ancors.forEach((item) => {
+        getScroll(item);
+      });
+
+    }
+
+    getSmoothScroll();
+
+
+  }, 2000)
+
+   //Открытие бургер-меню
+   const burger = document.querySelector('.menu-burge_root'),
+   menuBody = document.querySelector('.header__menu-body');
+
+ burger.addEventListener('click', () => {
+   burger.classList.toggle('menu-burger__active');
+   menuBody.classList.toggle('header__menu-body--show');
+ })
 
 })
 
