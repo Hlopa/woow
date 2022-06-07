6f3572d168a1da9a8ee775b90172a284ba20e1c2
Olga Barishevskaya <bariolga@yandex.ru>
2022-06-08 00:12:10 +0300

    add php form

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //sliders
   $('.reviews-slider').slick({
-    slidesToShow: 3.4,
+    slidesToShow: 5,
     slidesToScroll: 1,
     arrows: false,
     dots: false,
@@ -20,6 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
     pauseOnHover: false,
     accessibility: false,
     responsive: [
+      {
+        breakpoint: 2300,
+        settings: {
+          slidesToShow: 3.4,
+        }
+      },
       {
         breakpoint: 1380,
         settings: {
@@ -96,7 +102,7 @@ document.addEventListener("DOMContentLoaded", () => {
   $('.main-reviews-reviews-slider').slick({
     slidesToShow: 4,
     slidesToScroll: -1,
-    arrows:false,
+    arrows: false,
     dots: false,
     vertical: true,
     infinite: true,
@@ -110,8 +116,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
-
   setInterval(() => {
     //скролл и анимация
     let isScrolling = false;
@@ -119,6 +123,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let featuresFirstCat = featuresFirst.querySelector('.features_first-cat');
     let featuresFirstEye = featuresFirst.querySelector('.features_first-cat-eye');
     let featuresFirstEyeLines = featuresFirst.querySelector('.features_first-cat-eye-lines');
+
+    let featuresSecond = document.querySelector('.features_second');
+    let featuresSecondCat = featuresSecond.querySelector('.features_second-crasyCats');
 
     window.addEventListener("scroll", throttleScroll, false);
 
@@ -155,6 +162,10 @@ document.addEventListener("DOMContentLoaded", () => {
         addClasses(featuresFirstEye, ['animate__animated', 'animate__bounceIn', 'animate__delay-2s']);
         addClasses(featuresFirstEyeLines, ['animate__animated', 'animate__bounceIn', 'animate__delay-3s',]);
       }
+
+      if (isPartiallyVisible(featuresSecond, 150)) {
+        addClasses(featuresSecondCat, ['animate__animated', 'animate__slideInRight', 'animate__delay-1s']);
+      }
     }
 
 
@@ -162,9 +173,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const menuList = document.querySelector('.header__menu-list');
 
+
     const getSmoothScroll = () => {
       const ancors = menuList.querySelectorAll('a');
-     
+      const menuListItem = document.querySelectorAll('.header__menu-item');
+
       const getScroll = (element) => {
         element.addEventListener('click', function (e) {
           e.preventDefault();
@@ -182,6 +195,14 @@ document.addEventListener("DOMContentLoaded", () => {
         getScroll(item);
       });
 
+      menuListItem.forEach((item) => {
+        item.addEventListener('click', function (e) {
+          burger.classList.remove('menu-burger__active');
+          menuBody.classList.remove('header__menu-body--show');
+        })
+      });
+
+
     }
 
     getSmoothScroll();
@@ -189,14 +210,111 @@ document.addEventListener("DOMContentLoaded", () => {
 
   }, 2000)
 
-   //Открытие бургер-меню
-   const burger = document.querySelector('.menu-burge_root'),
-   menuBody = document.querySelector('.header__menu-body');
+  //Открытие бургер-меню
+  const burger = document.querySelector('.menu-burge_root'),
+    menuBody = document.querySelector('.header__menu-body');
 
- burger.addEventListener('click', () => {
-   burger.classList.toggle('menu-burger__active');
-   menuBody.classList.toggle('header__menu-body--show');
- })
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('menu-burger__active');
+    menuBody.classList.toggle('header__menu-body--show');
+  })
+
+
+  //выбор аватара в форме 
+
+  const avatars = document.querySelectorAll('.main-reviews-form-avatar-item');
+
+  avatars.forEach((avatar) => {
+    avatar.addEventListener('click', function (e) {
+      avatars.forEach((item) => {
+        item.classList.remove('main-reviews-form-avatar-item-active')
+      })
+      avatar.classList.toggle('main-reviews-form-avatar-item-active')
+    })
+  })
+
+  //modal
+
+  let btnOpen = document.querySelectorAll('.btn--open-modal');
+  let btnClose = document.querySelector('.btn--close-modal');
+  let modal = document.querySelector('.modal');
+
+
+  btnOpen.forEach((item) => {
+    item.addEventListener('click', function (e) {
+      modal.classList.add('modal--open');
+      burger.classList.remove('menu-burger__active');
+      menuBody.classList.remove('header__menu-body--show');
+    })
+  })
+
+  btnClose.addEventListener('click', function (e) {
+    modal.classList.remove('modal--open');
+  });
+
+
+
+  //отправка отзыва на почту
+
+  const form = document.getElementById('form');
+
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault()
+    let error = formValidate(form);
+
+    let formData = new FormData(form);
+
+    if (error === 0) {
+      form.classList.add('_sending')
+      let response = await fetch('sendmail.php', {
+        method: 'POST',
+        body: formData
+      })
+
+      if (response.ok) {
+        let result = await response.json();
+        alert(result.message);
+        form.reset();
+        form.classList.remove('_sending')
+      } else { 
+        alert('Ошибка')
+        form.classList.remove('_sending')
+       }
+    } else {
+      return
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+
+      if (input.value === '') {
+        formAddError(input)
+        error++;
+      }
+    }
+
+    return error
+
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add('_error-helper');
+    input.classList.add('_error');
+  }
+
+  function formRemoveError(input) {
+    input.parentElement.classList.remove('_error-helper');
+    input.classList.remove('_error');
+  }
+
 
 })
 
---
+++ app/sendmail.php
@@ -0,0 +1,45 @@
+<?php
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exeption;
+
+  require 'phpmailer/src/Exception.php';
+  require 'phpmailer/src/PHPMailer.php';
+
+  $mail = new PHPMailer(true);
+  $mail->CharSet = 'UTF-8';
+  $mail->setLanguage('ru', 'phpmailer/language/');
+  $mail->IsHTML(true);
+
+  //От кого письмо
+  $mail->setFrom('Woow.holistic');
+  //Кому отправить
+  $mail->addAddress('bariolga@yandex.ru')
+  //Тема письма
+  $mail->Subject = 'Форма обратной связи с Woow.holistic';
+
+  
+  //Тело письма
+  if(trim(!empty($_POST['name']))){
+    $body.='<p><strong>Имя:</strong>'.$_POST['name'].'</p>';
+  }
+
+  if(trim(!empty($_POST['descr']))){
+    $body.='<p><strong>Отзыв:</strong>'.$_POST['descr'].'</p>';
+  }
+
+  $mail->Body = $body;
+
+  //Отправялем
+  if(!$mail->send()){
+    $message = 'Ошибка';
+  } else {
+    $message = 'Данные отправлены!'
+  }
+
+  $response = ['message' => $message];
+
+  header('Content-type: application/json');
+  echo json_encode($response);
+
+  ?>
+
